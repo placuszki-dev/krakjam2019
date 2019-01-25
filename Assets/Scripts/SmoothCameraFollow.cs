8b3750606501d45efce0a5fd9ac1d88685560f88
Pastew <kpastew@gmail.com>
2019-01-25 22:05:59 +0100

    Camera Smooth Follow hero

---
+++ Assets/Scripts/SmoothCameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothCameraFollow : MonoBehaviour
+{
+
+    private Transform target;
+
+    public float followSpeed = 3f;
+
+    void Start()
+    {
+        target = FindObjectOfType<Hero>().transform;
+    }
+
+    void Update()
+    {
+        if (target)
+        {
+            Vector3 newPosition = target.position;
+            newPosition.z = -10;
+            transform.position = Vector3.Slerp(transform.position, newPosition, followSpeed * Time.deltaTime);
+        }
+        else
+        {
+            Debug.LogError("Cant find hero!!!!!");
+        }
+    }
+}
