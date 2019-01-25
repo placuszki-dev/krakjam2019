7d60a621c82c01175da635acf886f7105e3ed915
Pastew <kpastew@gmail.com>
2019-01-25 23:15:08 +0100

    Timer works. No visualization yet

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+
+    public float roundTime = 10f; // In seconds
+    private float timeLeft;
+
+    private PlayerManager playerManager;
+
+    void Awake()
+    {
+        playerManager = FindObjectOfType<PlayerManager>();
+        timeLeft = roundTime;
+    }
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+
+        if (timeLeft <= 0)
+            OnTimeLeft();
+    }
+
+    private void OnTimeLeft()
+    {
+        Debug.Log("Round time left");
+        playerManager.SwitchPlayers();
+        restartTimer();
+    }
+
+    public void restartTimer() {
+        timeLeft = roundTime;
+    }
+}
