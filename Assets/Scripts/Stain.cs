2e8f5468c08d4dbedd3a789b4abe5ba99a54180f
Pastew <kpastew@gmail.com>
2019-01-26 03:40:05 +0100

    more ligths and some objects'

---
+++ Assets/Scripts/Stain.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stain : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        print("Stain collision with " + col.gameObject.name);
+    }
+}
