178e579269297766650be307d0bbba88f18ba834
Pastew <kpastew@gmail.com>
2019-01-26 13:00:41 +0100

    Lighting script, cleanup

---
+++ Assets/Scripts/Lighting.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lighting : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Lighting hit " + other.name);
+    }
+}
