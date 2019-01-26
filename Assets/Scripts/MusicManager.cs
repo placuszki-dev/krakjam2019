9eb72c84bf8fe182edd061725f9449f42d8d4922
Pastew <kpastew@gmail.com>
2019-01-26 14:02:58 +0100

    Added music manager, music changer

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    internal void changeMusic(AudioClip newMusic)
+    {
+        audioSource.clip = newMusic;
+        audioSource.Play();
+    }
+}
