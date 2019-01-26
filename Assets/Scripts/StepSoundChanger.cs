9647d2c5001156d5fa0b3385bf477234e49f33bb
Pastew <kpastew@gmail.com>
2019-01-26 12:55:13 +0100

    StepSounds changer

---
+++ Assets/Scripts/StepSoundChanger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StepSoundChanger : MonoBehaviour
+{
+    public AudioClip stepsAudioClip;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (stepsAudioClip)
+            FindObjectOfType<Hero>().transform.GetComponentInChildren<StepsSound>().GetComponent<AudioSource>().clip = stepsAudioClip; // long line...
+    }
+}
