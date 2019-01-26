cdc095ee701fe366798559e65750aa33971a8430
Pastew <kpastew@gmail.com>
2019-01-26 01:56:09 +0100

    Flashlight

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    private Light light;
+
+    void Awake()
+    {
+        light = GetComponent<Light>();
+    }
+
+    public void Switch()
+    {
+        light.gameObject.SetActive(!light.isActiveAndEnabled);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     private Hero hero;
+    private Hand hand;
 
     public float speed = 10;
     public Color timerColor;
@@ -12,9 +14,29 @@ public class Player : MonoBehaviour
     void Start()
     {
         hero = FindObjectOfType<Hero>();
+        hand = FindObjectOfType<Hand>();
     }
 
     void FixedUpdate()
+    {
+        handleLeftAnalog();
+        handleRightAnalog();
+    }
+
+    private void handleRightAnalog()
+    {
+        float moveHorizontal = Input.GetAxis(gameObject.name + "HorizontalRight");
+        float moveVertical = Input.GetAxis(gameObject.name + "VerticalRight");
+        Vector3 dir = new Vector3(moveHorizontal * Time.deltaTime, moveVertical * Time.deltaTime);
+        if (dir.magnitude > 0.00001)
+        {
+            dir = dir.normalized;
+            float rot_z = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            hand.transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+        }
+    }
+
+    private void handleLeftAnalog()
     {
         float moveHorizontal = Input.GetAxis(gameObject.name + "Horizontal");
         float moveVertical = Input.GetAxis(gameObject.name + "Vertical");
@@ -33,3 +55,4 @@ public class Player : MonoBehaviour
         hero.transform.Translate(movement, Space.World);
     }
 }
+
