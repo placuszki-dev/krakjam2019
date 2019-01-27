1aed673f9e2b367ce2fde298bec5cd08b27a3aa7
Pastew <kpastew@gmail.com>
2019-01-27 15:02:04 +0100

    fix timer'

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -10,15 +10,17 @@ public class Timer : MonoBehaviour
     private float timeLeft;
 
     private PlayerManager playerManager;
-
-    public PillsIndicator pillsIndicator;
-    public RectTransform timerFill;
+    private PillsIndicator pillsIndicator;
 
     private bool canPlayChangePlayerEffect = true;
     public float secondsBeforeRoundEndStartChangePlayerAnimation = 2;
 
+    private PostProcessingEffectsManager postProcessingEffectsManager;
+
+
     void Awake()
     {
+        postProcessingEffectsManager = FindObjectOfType<PostProcessingEffectsManager>();
         playerManager = FindObjectOfType<PlayerManager>();
         pillsIndicator = FindObjectOfType<PillsIndicator>();
         timeLeft = roundTime;
@@ -28,6 +30,7 @@ public class Timer : MonoBehaviour
     {
         timeLeft -= Time.deltaTime;
         Visualize();
+        print(timeLeft);
 
         if (timeLeft <= 0)
             OnTimeLeft();
@@ -35,6 +38,9 @@ public class Timer : MonoBehaviour
         if (timeLeft <= secondsBeforeRoundEndStartChangePlayerAnimation && canPlayChangePlayerEffect)
         {
             canPlayChangePlayerEffect = false;
+
+            postProcessingEffectsManager.BloomBoom();
+            postProcessingEffectsManager.VignetteBoom();
         }
     }
 
@@ -53,14 +59,8 @@ public class Timer : MonoBehaviour
     {
         Debug.Log("Round time left");
         playerManager.SwitchPlayers();
-        ChangeTimerFillColor();
         restartTimer();
         canPlayChangePlayerEffect = true;
-    }
-
-    private void ChangeTimerFillColor()
-    {
-        timerFill.GetComponent<Image>().color = playerManager.GetActivePlayer().timerColor;
     }
 
     public void restartTimer() {
